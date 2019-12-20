7809f13a8738bb93f47b1901ca3d67846697f42c
yinyuxin <2636893464@qq.com>
2019-12-20 08:38:03 +0800

    1

---
+++ ZXLY.BOOK/ZXLY_DAL/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZXLY_DAL
+{
+    public class Model
+    {
+        public Book_information book {get;set;}
+        public User_info user { get; set; }
+        public Sales_list sales { get; set; }
+        public Borrowing_records borrowing { get; set; }
+    }
+}
---
+++ ZXLY.BOOK/ZXLY_DAL/biao/Sales_listgoumai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZXLY_DAL
+{
+    public partial class Sales_listgoumai
+    {
+        public int Slid { get; set; }
+        public int Bid { get; set; }
+        public int Id { get; set; }
+        public string purchase_time { get; set; }
+        public string Title { get; set; }
+        public string Warehousing { get; set; }
+        public int Borrowing_price { get; set; }
+        public int Unit_Price { get; set; }
+        public string Name { get; set; }
+    }
+}
